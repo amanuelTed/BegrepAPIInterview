fb02f7b6a67d8e83057ea2eacea1eb0c5aae7958
Amanuel TEDLA <amanuel.tedla@soprasteria.com>
2024-03-11 09:05:08 +0100

    Add base url to appsettings

--- BegrepAPI/Program.cs
+++ BegrepAPI/Program.cs
@@ -3,13 +3,15 @@ using BegrepAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var baseUrl = builder.Configuration.GetValue<string>("ApiSettings:BaseUrl");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddLogging();
 builder.Services.AddHttpClient<IConceptService, ConceptService>(client =>
 {
-    client.BaseAddress = new Uri("https://search.fellesdatakatalog.digdir.no/");
+    client.BaseAddress = new Uri(baseUrl);
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
